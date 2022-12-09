0281dd3293d2b065db33b6638aa9c2c0c1c05c74
carlospuigserver <cpuiggut@myuax.com>
2022-12-09 02:01:23 +0100

    ejercicio 3

--- Ejercicios/Ejercicio3.py
+++ Ejercicios/Ejercicio3.py
@@ -3,10 +3,27 @@ for i in range(11):
     lista.append(i)
 print(lista)
 
-
-# Todos los números del -10 al 0 [-10, -9, -8, ..., 0]
-
 Lista=[]
 for j in range(-10,1):
     Lista.append(j)
 print(Lista)
+
+Lista=[]
+for i in range(0,21):
+    if i%2==0:
+        Lista.append(i)
+print(Lista)
+
+LISta=[]
+for c in range(-20,1):
+    if c%2!=0:
+      LISta.append(c)
+print(LISta)
+
+
+Lista=[]
+for i in range(0,51):
+    if i%5==0:
+        Lista.append(i)
+print(Lista)
+
